c6bb9931b5148be801e6222c94326b4f90262b7d
Andy Holtzmann <designdroide@gmail.com>
2021-02-12 12:09:10 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -14,3 +14,11 @@ curl -sL --fail \
     -H "Authorization: Bearer ${GITHUB_PAT}" \
     -o "${RENAME}" \
     "$(cat asset.url)"
+
+tag=$(curl -sL --fail \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: Bearer ${GITHUB_PAT}" \
+    "https://api.github.com/repos/${REPO}/releases/${RELEASE}" \
+    | jq -r ".tag_name")
+    
+echo "::set-output name=release::$tag"
