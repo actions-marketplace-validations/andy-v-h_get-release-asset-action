ada9f11b73a2884e10f862f77e1882d3c6edc2a0
Andy Holtzmann <designdroide@gmail.com>
2021-02-12 12:26:50 -0700

    don't leave artifacts on the runner

--- download.sh
+++ download.sh
@@ -2,18 +2,17 @@
 
 set -e
 
-curl -sL --fail \
+asset=$(curl -sL --fail \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: Bearer ${GITHUB_PAT}" \
     "https://api.github.com/repos/${REPO}/releases/${RELEASE}" \
-    | jq -r ".assets | .[] | select(.name | test(\"${MATCH}\")) | .url" \
-    | tee asset.url
+    | jq -r ".assets | .[] | select(.name | test(\"${MATCH}\")) | .url" )
 
 curl -sL --fail \
     -H "Accept: application/octet-stream" \
     -H "Authorization: Bearer ${GITHUB_PAT}" \
     -o "${RENAME}" \
-    "$(cat asset.url)"
+    "$asset"
 
 tag=$(curl -sL --fail \
     -H "Accept: application/vnd.github.v3+json" \
